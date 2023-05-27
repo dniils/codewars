489b2fd075d259393399551765dd9144ac290059
Daniil Shitikov <tochka098@gmail.com>
2023-05-28 01:04:11 +0400

    add getterAndSetterOnExistingClass

---
+++ 6-kyu/getterAndSetterOnExistingClass/index.js
@@ -0,0 +1,29 @@
+class Person {
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  getName() {
+    return this.firstName + " " + this.lastName;
+  }
+}
+
+Object.defineProperties(Person.prototype, {
+  name: {
+    get() {
+      return this.firstName + " " + this.lastName;
+    },
+    set(name) {
+      this.firstName = name.split(" ")[0];
+      this.lastName = name.split(" ")[1];
+    },
+  },
+});
+
+let augustusCole = new Person("Augustus", "Cole");
+augustusCole.name = "Cole Train";
+console.log(augustusCole.firstName); // => 'Cole'
+console.log(augustusCole.lastName); // => 'Train'
+console.log(augustusCole.getName()); // => 'Cole Train'
+console.log(augustusCole.name); // => 'Cole Train'
